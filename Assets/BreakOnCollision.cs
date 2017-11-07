6f302194256431c1c3105af2d1ba6f2d670b8cec
WSEI\artur.lachor <a.lachor96@gmail.com>
2017-11-07 12:20:34 +0100

    First commit

---
+++ Assets/BreakOnCollision.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakOnCollision : MonoBehaviour
+{
+
+    void OnCollisionEnter()
+    {
+        Debug.Log("Collision!");
+        Invoke("PlatformDrop", 0.5f);
+    }
+
+    void PlatformDrop()
+    {
+        GetComponent<Rigidbody>().isKinematic = false;
+    }
+
+}
